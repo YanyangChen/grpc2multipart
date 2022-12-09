eea7a15436105827b9be6ba6fabda16cd56e011b
aaronchen <aaronchen9157@gmail.com>
2022-12-09 12:09:04 +0800

    testing http in a series

--- grpc/hello-camel-grpc-client/src/main/java/org/apache/camel/example/springboot/grpc/CamelHTTP2Route.java
+++ grpc/hello-camel-grpc-client/src/main/java/org/apache/camel/example/springboot/grpc/CamelHTTP2Route.java
@@ -16,6 +16,7 @@
  */
 package org.apache.camel.example.springboot.grpc;
 
+import org.apache.camel.BeanInject;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.Processor;
@@ -23,6 +24,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.netty.NettyServerBootstrapConfiguration;
 import org.apache.camel.examples.CamelHelloRequest;
 import org.apache.camel.examples.MimeContentRequest;
+import org.apache.camel.language.bean.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -41,7 +43,9 @@ public class CamelHTTP2Route extends RouteBuilder {
         //from uri="netty-http:http://0.0.0.0:{{port}}/foo?bootstrapConfiguration=#nettyHttpBootstrapOptions"
 
 	//setup endpoint and its behavior
-        from("timer://foo?period=5000&repeatCount=1").process(new Processor() {
+        from("timer://foo?period=5000&repeatCount=1")
+                .routeId("http2start")
+                .process(new Processor() {
             @Override
 	    //set the body with the object, using the interface with thr object and its type name
             public void process(Exchange exchange) throws Exception {
@@ -55,45 +59,27 @@ public class CamelHTTP2Route extends RouteBuilder {
                 .convertBodyTo(String.class)
                 .setExchangePattern(ExchangePattern.InOut)
                 //.to("netty-http:http://0.0.0.0:9000/foo");
-                .to("seda:netty-http:http://0.0.0.0:9000/foo");
+                .to("seda:netty-http:http://0.0.0.0:9000/foo")
+                .to("mock:start");
+
 
         from("seda:netty-http:http://0.0.0.0:9000/foo")
-                //.transform().constant("Bye World");
-                //.log("Message body back in grpc: ${body}")
-
-                //.to("activemq:my-activemq-grpc2Str")
-
+                .routeId("http2middle")
                 .marshal()
                 .mimeMultipart("mixed", true, true, "(included|x-.*)", true)
-                //.mimeMultipart()
-                //front plat netty end
-
-                //send to another netty server
-                //.to("netty-http:http://0.0.0.0:8123/foo") //send to assumed netty server port 8123
-                //.log("Message body back in multipart: ${body}")
                 .to("seda:netty-http:http://0.0.0.0:8123/foo")
-                .log("Message body back multipart: ${body}");
+                .log("Message body back multipart: ${body}")
+                .to("mock:middle");
 
         from("seda:netty-http:http://0.0.0.0:8123/foo")
-                //.log("Message body back in multipart: ${body}") //multipart once logged the body will be empty
-                //.convertBodyTo(String.class)
-                //
-                //
-                //
-                //
-                //convert to a more generic grpc type then send to server
-                //not convert directly to the ending type, that's black box's job
+                .routeId("http2end")
                 .process(exchange -> {
                     String receivedBody = exchange.getIn().getBody(String.class);
-
-                    // log.info("Message body back in multipart " + receivedBody);
-
                     MimeContentRequest request2 = MimeContentRequest.newBuilder().setContent(receivedBody).build();
                     exchange.getIn().setBody(request2, MimeContentRequest.class);
                     log.info("Message body back in grpc " + exchange.getIn().getBody());
                 })
-                //.to("seda:netty-http:http://0.0.0.0:8124/foo")
-                //.log("Message body back in grpc: ${body}")
+                .to("mock:end")
                 .to("log:org.apache.camel.example?level=INFO");
 
     }
