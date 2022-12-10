b30f6395e22e6fe54a8d4e8a2c5da41d318e9465
aaronchen <aaronchen9157@gmail.com>
2022-12-10 15:57:56 +0800

    adding a 4th test

--- grpc/hello-camel-grpc-test/testScript.sh
+++ grpc/hello-camel-grpc-test/testScript.sh
@@ -1,6 +1,8 @@
-mvn test -Dtest=SimpleHttp2Test#test0*;
-mvn test -Dtest=SimpleHttp2Test#test1*;
-mvn test -Dtest=SimpleHttp2Test#test2*;
-mvn test -Dtest=SimpleHttp2Test#test3*;
+echo "================== testing results ==================" > results;
+mvn test -Dtest=SimpleHttp2Test#test0* | tee >>  results; echo "test0 : \n"; cat results | grep "Time elapsed:" | tail -n 1;
+mvn test -Dtest=SimpleHttp2Test#test1* | tee >>  results; echo "test1 : \n"; cat results | grep "Time elapsed:" | tail -n 1;
+mvn test -Dtest=SimpleHttp2Test#test2* | tee >>  results; echo "test2 : \n"; cat results | grep "Time elapsed:" | tail -n 1;
+mvn test -Dtest=SimpleHttp2Test#test3* | tee >>  results; echo "test3 : \n"; cat results | grep "Time elapsed:" | tail -n 1;
+mvn test -Dtest=SimpleHttp2Test#test4* | tee >>  results; echo "test4 : \n"; cat results | grep "Time elapsed:" | tail -n 1;
 
 
